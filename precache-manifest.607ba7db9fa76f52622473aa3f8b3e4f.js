5f9165911664340eb56b3d60b79353021f887a06
Lee Barton <hleebarton97@gmail.com>
2019-09-13 12:50:26 -0500

    Updates

---
+++ precache-manifest.607ba7db9fa76f52622473aa3f8b3e4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d59799b408e5c1344d4bd199aac41a",
+    "url": "/primer/index.html"
+  },
+  {
+    "revision": "7e3dc54dfa675fc9619c",
+    "url": "/primer/static/css/main.db8700f0.chunk.css"
+  },
+  {
+    "revision": "e2508e5b5291e7ad1f37",
+    "url": "/primer/static/js/2.af6e74e4.chunk.js"
+  },
+  {
+    "revision": "7e3dc54dfa675fc9619c",
+    "url": "/primer/static/js/main.32746554.chunk.js"
+  },
+  {
+    "revision": "a571426ff3781cb70be7",
+    "url": "/primer/static/js/runtime~main.b83768aa.js"
+  }
+]);
